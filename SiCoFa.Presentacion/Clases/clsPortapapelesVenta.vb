b9f5f11679d8b27ea1e90458cf2fdbc2ce40bff9
vechobetian <nelsonbetian@gmail.com>
2025-05-26 19:25:07 -0300

    SEGUNDA ACTUALIZACION RESISTENCIA 26-05-25

--- SiCoFa.Presentacion/Clases/clsPortapapelesVenta.vb
+++ SiCoFa.Presentacion/Clases/clsPortapapelesVenta.vb
@@ -3,6 +3,23 @@ Imports System.ComponentModel
 
 Public Class clsPortapapelesVenta
     Public Shared Operacion As Operacion
-    Public Shared Items As BindingList(Of ItemComprobante)
     Public Shared Cliente As Cliente
+
+    Private Shared _Items As BindingList(Of ItemComprobante)
+    Public Shared Property Items As BindingList(Of ItemComprobante)
+        Get
+            Return _Items
+        End Get
+        Set(value As BindingList(Of ItemComprobante))
+            _Items = value
+
+            ' Lógica automática: establecer IdItem = 0 en todos los ítems
+            If _Items IsNot Nothing Then
+                For Each item As ItemComprobante In _Items
+                    item.IdItem = 0
+                Next
+            End If
+        End Set
+    End Property
+
 End Class
