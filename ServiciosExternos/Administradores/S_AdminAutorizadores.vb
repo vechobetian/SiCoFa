6b380e342caebca2b162ef372426ceb88b945bcb
vechobetian <nelsonbetian@gmail.com>
2026-07-04 13:26:02 -0300

    NB26070401

---
+++ ServiciosExternos/Administradores/S_AdminAutorizadores.vb
@@ -0,0 +1,24 @@
+﻿Public NotInheritable Class S_AdminAutorizadores
+    Private Sub New()
+    End Sub
+
+    Public Shared Function Crear(nombreValidador As String) As IValidador
+
+        Select Case nombreValidador.ToUpper()
+
+            Case "LPAMI"
+                Return New LPAMI()
+
+            Case "FLINK"
+                Return New FLINK
+
+            Case "MISV"
+                Return New MISV
+
+            Case Else
+                Throw New Exception("Validador no implementado: " & nombreValidador)
+
+        End Select
+
+    End Function
+End Class
---
+++ ServiciosExternos/Autorizadores/FLINK.vb
@@ -0,0 +1,3 @@
+﻿Public Class FLINK
+
+End Class
